c2fc6b32c5fd391988aa1e6bbeb6727380f2a823
서준규 <thddlwnsrb@naver.com>
2025-10-23 12:11:42 +0900

    적 추적, 공격, 스크립터블 오브젝트 추가

---
+++ Assets/Scripts/Enemy/EnemyDatas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDatas", menuName = "Scriptable Objects/EnemyDatas")]
+public class EnemyDatas : ScriptableObject
+{
+    public float maxHp = 100f;
+    public float damage = 20f;
+    public float speed = 3.5f;
+    public float traceDistance;
+    public float attackDistance;
+
+    public EnemyType type = EnemyType.Melee;
+}
---
+++ Assets/Scripts/playerTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class playerTest : Living
+{
+    public override void OnDamage(float damage)
+    {
+        base.OnDamage(damage);
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
